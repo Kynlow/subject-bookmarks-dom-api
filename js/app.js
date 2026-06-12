e522ee57d4a15552ed351e8d0d82ca038e9de602
Kynlow <elwen@areastudio.fr>
2026-06-12 10:08:49 +0200

    feat: finito

---
+++ js/app.js
@@ -0,0 +1,61 @@
+const $form = document.querySelector("#favori-form");
+const $ul = document.querySelector("ul");
+
+let timerInterval = null;
+
+document.addEventListener("DOMContentLoaded", interval());
+
+$form.addEventListener("submit", (e) => {
+    e.preventDefault()
+	const formdata = new FormData($form);
+	const title = formdata.get("titre");
+	const url = formdata.get("url");
+	const description = formdata.get("description");
+
+	addDatasOnAPI(title, url, description);
+});
+
+function addDatasOnPage(title, url, description) {
+	const $li = document.createElement("li");
+	$li.innerHTML = `<a href="${url}" target="_blank">${title}</a>
+            <p class="description">
+              ${description}
+            </p>`;
+	$ul.appendChild($li);
+}
+
+async function addDatasOnAPI(titleContent, urlContent, descriptionContent) {
+	const request = await fetch(`http://10.69.4.8:8100/bookmarks`, {
+		method: "POST",
+		headers: {
+			Authorization: "Bearer 2495022d236a639a59f3b149b6cf60eb",
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({
+			title: titleContent,
+			url: urlContent,
+			description: descriptionContent,
+		}),
+	});
+
+	addDatasOnPage(titleContent, urlContent, descriptionContent);
+}
+
+async function getDatasOnAPI() {
+	const request = await fetch("http://10.69.4.8:8100/bookmarks");
+	const response = await request.json();
+	$ul.innerHTML = "";
+	for (let i = 0; i < response.length; i++) {
+		addDatasOnPage(
+			response[i].title,
+			response[i].url,
+			response[i].description,
+		);
+	}
+}
+
+async function interval() {
+	timerInterval = setInterval(() => {
+		getDatasOnAPI();
+	}, 3000);
+}
